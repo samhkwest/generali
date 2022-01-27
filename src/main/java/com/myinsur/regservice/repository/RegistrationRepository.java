c3aaf71cd31e8629926df699b69a4c3b48277912
samhkwest <samhkwest@gmail.com>
2022-01-27 23:31:56 +0800

    first commit

---
+++ src/main/java/com/myinsur/regservice/repository/RegistrationRepository.java
@@ -0,0 +1,23 @@
+package com.myinsur.regservice.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.myinsur.regservice.model.Registration;
+
+@Repository
+public interface RegistrationRepository extends JpaRepository<Registration, Long>, ValidatorRespository {
+		
+	public Optional<Registration> findById(long id);
+	
+	public List<Registration> findAll();
+	
+	public Registration saveAndFlush(Registration registration);
+	
+	public void deleteAll();
+	
+
+}
